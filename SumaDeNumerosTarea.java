eee9b1e48381afdf6d1987fad93b9796cdb15a09
Chong684 <gabrielgutierrezerick3@gmail.com>
2025-01-21 18:18:24 -0700

    Add files via upload

---
+++ SumaDeNumerosTarea.java
@@ -0,0 +1,16 @@
+public class SumaDeNumerosTarea {
+    public static void main(String[] args) {
+        int numero1 = 2;
+        int numero2 = 4;
+        
+        int suma = numero1 + numero2;
+        
+        System.out.println(" la suma " + numero1 + " y " + numero2 + " es igyual a: " + suma);
+        
+        int resta = numero1 - numero2;
+        
+        System.out.println(" la resta " + numero1 + " y " + numero2 + " es igual a: " + resta);
+    }
+}
+
+//ezzz
